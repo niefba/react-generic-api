e945de68d1ea8ac1780a06cebaa854f8f271accc
niefba <fabien.tillon@rupil.fr>
2025-03-03 15:21:11 +0100

    Create Dialog component

--- src/App.tsx
+++ src/App.tsx
@@ -2,33 +2,55 @@ import './App.css';
 import {useState} from 'react';
 import {FirstComponent} from './ui/first-component';
 import {SecondComponent} from './ui/second-component';
-import { Dialog, DialogHeader, DialogBody, DialogFooter } from './ui/dialog';
-
+import { Dialog } from './ui/dialog';
 
 function App() {
   const [showDialog, setShowDialog] = useState(false);
+  const [showDialogModal, setShowDialogModal] = useState(false);
+
+
   return (
-    <>
+    <div>
+      
       <h1 className="text-3xl font-bold m-2">Different generic APIs and components</h1>
+      
       <FirstComponent />
       <SecondComponent />
-      <button onClick={() => setShowDialog(true)}>
-        Show modal without a portal
+
+      <button className='rounded-2xl bg-gray-600 mx-2 px-4 text-white not-disabled:cursor-pointer disabled:bg-gray-400'
+        onClick={() => setShowDialog(!showDialog)}>
+        Show regular dialog
+      </button>
+      <button className='rounded-2xl bg-gray-600 mx-2 px-4 text-white not-disabled:cursor-pointer disabled:bg-gray-400'
+        onClick={() => setShowDialogModal(true)}>
+        Show modal dialog
       </button>
       
-      <Dialog modal={true} showDialog={showDialog}>
-        <DialogHeader>
-          <h2 className="text-xl font-bold">Dialog Header</h2>
-        </DialogHeader>
-        <DialogBody>
-          <p>Dialog Body</p>
-        </DialogBody>
-        <DialogFooter onClose={() => setShowDialog(false)}>
-          <p>Dialog Footer</p>
-        </DialogFooter>
+      <Dialog modal={false} isOpen={showDialog} header={<strong>Regular Dialog Header</strong>} footer={<i>Dialog Footer</i>} onClose={() => setShowDialog(false)}>
+          <FirstComponent />
       </Dialog>
       
-    </>
+      <Dialog modal={true} isOpen={showDialogModal} header={<strong>Modal Dialog Header</strong>} footer={<i>Dialog Footer</i>} onClose={() => setShowDialogModal(false)}>
+          <FirstComponent />
+      </Dialog>
+
+      
+      
+      <p>Some content</p>
+      <p>Some content</p>
+      <p>Some content</p>
+      <p>Some content</p>
+      <p>Some content</p>
+      <p>Some content</p>
+      <p>Some content</p>
+      <p>Some content</p>
+      <p>Some content</p>
+      <p>Some content</p>
+      <p>Some content</p>
+      <p>Some content</p>
+      <p>Some content</p>
+      <p>Some content</p>
+    </div>
   )
 }
 
--- src/ui/dialog.tsx
+++ src/ui/dialog.tsx
@@ -1,33 +1,41 @@
-export function Dialog({modal, showDialog, children}: {modal: boolean, showDialog: boolean, children: React.ReactNode}) {
-    return (
-        <>
-        { modal && showDialog &&
-            <div>{children}</div>
-        }
-        </>
-    )
+type Props = {
+    modal: boolean,
+    isOpen: boolean,
+    header: React.ReactNode,
+    footer: React.ReactNode,
+    onClose: React.MouseEventHandler,
+    children: React.ReactNode
 }
 
-export function DialogHeader({children}: {children: React.ReactNode}) {
-    return (
-        <div>{children}</div>
-    )
-}
-
-export function DialogBody({children}: {children: React.ReactNode}) {
-    return (
-        <div>{children}</div>
-    )
-}
-
-export function DialogFooter({children, onClose}: {children: React.ReactNode, onClose: () => void}) {
-    return (
-        <div>
-            {children}
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
-                onClick={onClose}>
-                Close
-            </button>
-        </div>
-    )
+export function Dialog({modal, isOpen, header, footer, onClose, children}: Props){
+        return (
+            <>
+            { isOpen &&
+                <div className={`modal ${ modal ? 'fixed items-center' : 'relative'} w-full h-full top-0 left-0 flex justify-center`}>
+                    { modal && <div className="modal-overlay absolute w-full h-full bg-gray-900 opacity-50"></div>}
+                    <div className="modal-container absolute bg-white w-11/12 md:max-w-md mx-auto rounded shadow-lg z-50 overflow-y-auto">
+                        <div className="modal-content py-4 px-6">
+                            <div>
+                                <button
+                                    className="float-right cursor-pointer"
+                                    onClick={onClose}>
+                                    X
+                                </button>
+                                {header}
+                            </div>
+                            <div>{children}</div>
+                            <div>{footer}</div>
+                            <div className="flex justify-center mt-2">
+                                <button
+                                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded cursor-pointer"
+                                    onClick={onClose}>
+                                    Close
+                                </button>
+                            </div>
+                        </div> 
+                    </div>
+                </div>
+            }
+            </>
+        )
 }
